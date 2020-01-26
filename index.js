adfe29afa0789aa2cd0c8319e44ca2f347d875b7
taneja09 <taneja.di@northeastern.edu>
2020-01-26 00:40:25 -0500

    bill create completed

--- index.js
+++ index.js
@@ -1,23 +1,85 @@
-const connection = require ('./db-config/databaseConfig');
+// const sequelize = require ('./db-config/databaseConfig');
+const models = require('./models');
 const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors');
+var port = normalizePort(process.env.PORT || '3000');
+
 
 const app = express();
+app.set('port',port);
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/',(req,res) => {
-    res.json({'message':'Welcome'}); 
+    res.json({'message':'Hello Cloud !!'}); 
 });
 
 require('./api-routes/routes')(app);
 
-connection.connect();
 
-global.db = connection;
 
-const port = 3000;
-app.listen(port,() => {
-    console.log("Server Running on port"+" "+port);
-});
+  models.sequelize.sync().then(function() {
+    /**
+     * Listen on provided port, on all network interfaces.
+     */
+    app.listen(port, function() {
+      console.log('Express server listening on port ' + port);
+    });
+    app.on('error', onError);
+    app.on('listening', onListening);
+  });
+  
+
+  function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+
+
+  function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+  
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  
+  function onListening() {
+    var addr = app.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    console.log('Listening on ' + bind);
+  }
---
+++ models/bill.js
@@ -0,0 +1,52 @@
+'use strict';
+const uuid = require('uuid/v4');
+module.exports = (sequelize, DataTypes) => {
+var Bill = sequelize.define('Bill', {
+    id: {
+        allowNull: false,
+        primaryKey: true,
+        unique:true,
+        type: DataTypes.UUID
+    },
+    created_ts:{
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    updated_ts:{
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    vendor:{
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    bill_date:{
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    due_date:{
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    amount_due:{
+        allowNull: false,
+        type: DataTypes.DOUBLE,
+        min: 0.01
+    },
+    categories: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    paymentStatus:{
+        allowNull: false,
+        type: DataTypes.ENUM('paid', 'due', 'past_due', 'no_payment_required')
+    }
+},
+    {
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'singularName'
+    });
+
+return Bill;
+}
