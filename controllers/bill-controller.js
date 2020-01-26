359dbf9d25f65f549c76828b6d1be3fb41cc15d7
taneja09 <taneja.di@northeastern.edu>
2020-01-26 02:53:55 -0500

    Bill API completed

--- controllers/bill-controller.js
+++ controllers/bill-controller.js
@@ -78,3 +78,231 @@ exports.create = (req, res) => {
             });
     }
 }
+
+exports.viewAllBills = (req, res) => {
+    var credentials = auth(req);
+    if (!credentials) {
+        console.log("hello");
+        res.statusCode = 401
+        res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+        res.end('Access denied')
+    } else {
+        var username = credentials.name;
+        var password = credentials.pass;
+        models.User.findAll({
+                where: {
+                    email_address: username
+                }
+            }).then(function(result) {
+                var valid = true;
+                valid = bcrypt.compareSync(password, result[0].password) && valid;
+                if (valid) {
+                    models.Bill.findAll({
+                        where: {
+                            owner_id: result[0].id
+                        }
+                    }).then(function(UserBills){
+                        res.status(200).send(UserBills);
+                    }).catch(function(err){
+                        console.log(err);
+                    });
+
+                }else{
+                    res.statusCode = 401
+                    res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                    res.end('Access denied')
+                }
+
+            }).catch(function(err){
+                res.statusCode = 401
+                    res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                    res.end('Access denied')
+            });
+
+        }   
+
+    }
+
+    exports.getBill = (req, res) => {
+        var credentials = auth(req);
+        if (!credentials) {
+            console.log("hello");
+            res.statusCode = 401
+            res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+            res.end('Access denied')
+        } else {
+            var username = credentials.name;
+            var password = credentials.pass;
+            var billId = req.url.split("/")[3];
+            models.User.findAll({
+                where: {
+                    email_address: username
+                }
+            }).then(function(result) {
+                var valid = true;
+                valid = bcrypt.compareSync(password, result[0].password) && valid;
+                if (valid) {
+                    models.Bill.findAll({
+                        where: {
+                            id: billId
+                        }
+                    }).then(function(UserBill) {
+                        if (UserBill.length > 0)
+                            res.status(200).send(UserBill);
+    
+                        else
+                            res.status(404).end();
+    
+                    }).catch(function(err) {
+                        console.log(err);
+                    });
+    
+                } else {
+                    res.statusCode = 401
+                    res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                    res.end('Access denied')
+                }
+            }).catch(function(err) {
+                res.statusCode = 401
+                res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                res.end('Access denied')
+            });
+    
+        }
+    
+    }
+
+    exports.updateBill = (req, res) => {
+            var credentials = auth(req);
+            if (!credentials) {
+                console.log("hello");
+                res.statusCode = 401
+                res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                res.end('Access denied')
+            } else {
+                var username = credentials.name;
+                var password = credentials.pass;
+                var billId = req.url.split("/")[3];
+                models.User.findAll({
+                        where: {
+                            email_address: username
+                        }
+                    }).then(function(result) {
+                        var valid = true;
+                        valid = bcrypt.compareSync(password, result[0].password) && valid;
+                        if (valid) {
+        
+                            var vendor = req.body.vendor;
+                            var amount_due = req.body.amount_due;
+                            var categories = req.body.categories;
+                            categories = categories.join();
+                            var paymentStatus = req.body.paymentStatus;
+                            var bill_date = req.body.bill_date;
+                            var due_date = req.body.due_date;
+                            var datevalts = new Date();
+                            datevalts = datevalts.toISOString();
+        
+                            if (!vendor || !bill_date || !due_date || !amount_due || !categories || !paymentStatus) {
+                                res.status(400).send({
+                                    Message: "Please provide all required fields - vendor, bill_date, due_date, amount_due, categories, paymentStatus !"
+                                });
+                            } else if (isNaN(amount_due) || amount_due < 0.01) {
+                                res.status(400).send({
+                                    Message: "Please enter correct amount and it should be greater than 0.01 "
+                                });
+                            } else {
+                                models.Bill.update({
+                                    updated_ts: datevalts,
+                                    vendor: vendor,
+                                    bill_date: bill_date,
+                                    due_date: due_date,
+                                    amount_due: amount_due,
+                                    categories: categories,
+                                    paymentStatus: paymentStatus
+                                }, {
+                                    where: {
+                                        id: billId
+                                    }
+                                }).then(function(BillUpdate) {
+                                    if (BillUpdate[0] > 0) {
+                                        models.Bill.findOne({
+                                            where: {
+                                                id: billId
+                                            }
+                                        }).then(function(updatedBill) {
+                                            res.status(200).send(updatedBill);
+                                        }).catch(function(err) {
+                                            console.log(err);
+                                        });
+                                    } else {
+                                        res.status(404).end();
+                                    }
+                                }).catch(function(err) {
+                                    console.log(err);
+                                    res.status(400).send("Issue while updating the Bill !");
+                                });
+        
+                            }
+                        } else {
+                            res.statusCode = 401
+                            res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                            res.end('Access denied')
+                        }
+                    })
+                    .catch(function(err) {
+                        console.log(err);
+                        res.statusCode = 401
+                        res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                        res.end('Access denied')
+                    });
+            }
+        }
+
+        exports.deleteBill = (req, res) => {
+            var credentials = auth(req);
+            if (!credentials) {
+                console.log("hello");
+                res.statusCode = 401
+                res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                res.end('Access denied')
+            } else {
+                var username = credentials.name;
+                var password = credentials.pass;
+                var billId = req.url.split("/")[3];
+                models.User.findAll({
+                    where: {
+                        email_address: username
+                    }
+                }).then(function(result) {
+                    var valid = true;
+                    valid = bcrypt.compareSync(password, result[0].password) && valid;
+                    if (valid) {
+                        models.Bill.destroy({
+                            where: {
+                                id: billId
+                            }
+                        }).then(function(UserBill) {
+                            if (UserBill > 0)
+                                res.status(204).end();
+        
+                            else
+                                res.status(404).end();
+        
+                        }).catch(function(err) {
+                            console.log(err);
+                        });
+        
+                    } else {
+                        res.statusCode = 401
+                        res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                        res.end('Access denied')
+                    }
+                }).catch(function(err) {
+                    res.statusCode = 401
+                    res.setHeader('WWW-Authenticate', 'Basic realm="user Authentication"')
+                    res.end('Access denied')
+                });
+        
+            }
+        
+        }
