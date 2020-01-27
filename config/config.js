fbecd31c896d9990ba94238cb75e3fb9553a7d46
taneja09 <taneja.di@northeastern.edu>
2020-01-27 15:41:12 -0500

    changed doecker img for DB

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
-{
+require('dotenv').config();
+module.exports = {
   "development": {
     "username": "root",
     "password": "divinity@1202",
@@ -8,11 +9,12 @@
     "logging": false
   },
   "test": {
-    "username": "root",
-    "password": "divinity@1202",
-    "database": "CloudDB",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.MYSQL_ROOT_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "dialect": process.env.MYSQL_DIALECT,
+    "logging": false
   },
   "production": {
     "username": "root",
