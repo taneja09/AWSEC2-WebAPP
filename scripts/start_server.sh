00bddc2dc47a8dd76592ce4f2e158452fb635596
taneja09 <taneja.di@northeastern.edu>
2020-03-04 15:46:35 -0500

    edited appspec.yml

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
-crontab -l | { cat; echo "@reboot pm2 start /temp/webapp/index.js -i 0 --name \"webapp\""; } | crontab -
+crontab -l | { cat; echo "@reboot pm2 start /var/webapp/index.js -i 0 --name \"webapp\""; } | crontab -
 sudo pm2 stop webapp
 # actually start the server
-sudo pm2 start /var/www/index.js -i 0 --name "webapp"
+sudo pm2 start /var/webapp/index.js -i 0 --name "webapp"
