64e308290d708c2bb67787f3e06dd15a51cc9faf
taneja09 <taneja.di@northeastern.edu>
2020-03-05 00:33:08 -0500

    added path to shell scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,6 @@
 
 # This script is used to start the application
 cd /home/ubuntu/cddemo
-sudo source ./etc/profile.d/myvariables.sh
+sudo -s source ./etc/profile.d/myvariables.sh
 printenv
 node index.js
