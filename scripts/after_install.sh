c88191a4377cc216e4b1a3a3c987300557d917f5
taneja09 <taneja.di@northeastern.edu>
2020-03-05 11:02:06 -0500

    added path to shell scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,9 +2,11 @@
 #!/bin/bash
 
 # This script is executed after the source is copied to the instances
+
 cd /
-. ./etc/myvariables.sh
-printenv
+cp /myvariables.sh /home/ubuntu/cddemo/scripts/myvariables.sh
 cd /home/ubuntu/cddemo
+chmod +x /scripts/myvariables.sh
+. ./scripts/myvariables.sh
 printenv
 npm install
