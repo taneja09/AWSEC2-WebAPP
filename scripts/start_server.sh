a3cca5981bbda6bd3599802875bf37b3abc92f35
taneja09 <taneja.di@northeastern.edu>
2020-03-04 19:52:08 -0500

    added path to shell scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
-forever stopall
 # actually start the server
 forever start /home/ubuntu/nodejs/index.js
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pm2 stop webapp
+forever stopall
