44e9de7e60b9fe5c1d9ff283d22f5560c5c6052e
taneja09 <taneja.di@northeastern.edu>
2020-03-05 10:45:43 -0500

    added path to shell scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # This script is executed after the source is copied to the instances
-sudo -s source ./opt/myvariables.sh
+. ./etc/myvariables.sh
 cd /home/ubuntu/cddemo
 printenv
 npm install
