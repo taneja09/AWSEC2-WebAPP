a3c44e4a0a4f07315d5c5f6a6861b3e669bf940d
taneja09 <taneja.di@northeastern.edu>
2020-03-05 00:42:45 -0500

    added path to shell scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,6 @@
 
 # This script is executed after the source is copied to the instances
 cd /home/ubuntu/cddemo
+sudo -s source ./etc/profile.d/myvariables.sh
+printenv
 npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,4 @@
 
 # This script is used to start the application
 cd /home/ubuntu/cddemo
-sudo -s source ./etc/profile.d/myvariables.sh
-printenv
 node index.js
