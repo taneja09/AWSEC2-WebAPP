b2a4de39e4f62d8ce8295e043dd2c831692dce56
taneja09 <taneja.di@northeastern.edu>
2020-03-11 17:34:37 -0400

    changed the deploy to master

--- test/functional/testfile.js
+++ test/functional/testfile.js
@@ -1,6 +1,3 @@
-// var request = require('superagent');
-// var serverUrl = "http://localhost:3000/v1/user";
-
 const request = require('supertest');
 const express = require('express');
 var bodyParser = require('body-parser')
@@ -27,20 +24,3 @@ describe('Post /v1/user', function(req,res) {
           });
     });
   });
-
-
-// describe('Creation Tests', function() {
-//     it("Creates a new user in Database.", function(done) {
-//         request.post(serverUrl).send({
-//             "first_name": "dice",
-//             "last_name": "roller",
-//             "password": "abcdefduyt",
-//             "email_address": "dicemedia@gmail.com"
-//         }).end(function(err, res) {
-//             if (err) {
-//                 console.log(err);
-//             } else
-//                 done();
-//         })
-//     })
-// });
