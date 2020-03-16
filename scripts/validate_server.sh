3855c0edcc02c8e768bb6ab77acd6b3bc7742439
taneja09 <taneja.di@northeastern.edu>
2020-03-15 22:44:45 -0400

    added metrics statsD for all APIs

--- scripts/validate_server.sh
+++ scripts/validate_server.sh
@@ -6,12 +6,14 @@ listencount=$(netstat -an | grep 3000 | grep LISTEN | wc -l)
 if [ "$listencount" -lt 1 ]; then
     exit 1
 else
+    cd /home/ubuntu/cddemo
+    node node_modules/statsd/stats.js app-metrics/statsd-config.json
+    echo "***************Reloading the cloudwatch service**************"
+
     sudo systemctl reload-or-restart cloudwatch.service
     echo "***************Reloading the cloudwatch service**************"
 
-    cd /home/ubuntu/cddemo
-    node node_modules/statsd/stats.js app-metrics/statsd-config.json > /dev/null 2> /dev/null < /dev/null &
-    echo "***************Reloading the cloudwatch service**************"
+
 
     exit 0
 fi
