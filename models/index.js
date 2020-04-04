f4158d30e6714376e19a30e6b6de4cfb2608377d
taneja09 <taneja.di@northeastern.edu>
2020-04-04 05:14:46 -0400

    added SSL for DB connection

--- models/index.js
+++ models/index.js
@@ -7,13 +7,18 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV||"development";
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
+const rdsCa = fs.readFileSync(__dirname + '/../certificates/rds-combined-ca-bundle.pem');
 
 let sequelize;
 console.log(config);
 // if (config.env == "test") {
 //   sequelize = new Sequelize(process.env[config.env], config);
 // } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config,{
+    dialectOptions: {
+      ssl: 'Amazon RDS'
+    }
+  });
 //}
 
 fs
